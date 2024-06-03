d56402a49d252f4f16e160641016ac8dc95f4488
Paul Ohno <pohno@users.noreply.github.com>
2024-06-03 09:27:35 -0500

    pm and voc from 2023

---
+++ SF_basic_VOC/SF_basic_VOC.ino
@@ -0,0 +1,73 @@
+/* example1_basic.ino
+
+ This example shows basic data retrieval from the SparkFun Indoor Air Quality Sensor - ENS160.
+
+ Written by: 
+	Elias Santistevan @ SparkFun Electronics October, 2022
+
+ Product: 
+  https://www.sparkfun.com/products/20844
+ 
+ Repository:
+	https://github.com/sparkfun/SparkFun_Indoor_Air_Quality_Sensor-ENS160_Arduino_Library
+
+ SparkFun code, firmware, and software is released under the MIT
+ License(http://opensource.org/licenses/MIT).
+
+*/
+#include <Wire.h>
+#include "SparkFun_ENS160.h"
+
+SparkFun_ENS160 myENS; 
+
+int ensStatus; 
+
+void setup()
+{
+	Wire.begin();
+
+	Serial.begin(115200);
+
+	if( !myENS.begin() )
+	{
+		//Serial.println("Did not begin.");
+		while(1);
+	}
+
+	// Reset the indoor air quality sensor's settings.
+	if( myENS.setOperatingMode(SFE_ENS160_RESET) )
+		//Serial.println("Ready.");
+
+	delay(100);
+
+	// Device needs to be set to idle to apply any settings.
+	// myENS.setOperatingMode(SFE_ENS160_IDLE);
+
+	// Set to standard operation
+	// Others include SFE_ENS160_DEEP_SLEEP and SFE_ENS160_IDLE
+	myENS.setOperatingMode(SFE_ENS160_STANDARD);
+
+	// There are four values here: 
+	// 0 - Operating ok: Standard Operation
+	// 1 - Warm-up: occurs for 3 minutes after power-on.
+	// 2 - Initial Start-up: Occurs for the first hour of operation.
+  //												and only once in sensor's lifetime.
+	// 3 - No Valid Output
+	ensStatus = myENS.getFlags();
+	//Serial.print("Gas Sensor Status Flag: ");
+	//Serial.println(ensStatus);
+	
+}
+
+void loop()
+{
+	if( myENS.checkDataStatus() )
+	{
+
+		Serial.print("Total_VOC_[ppb]:");
+		Serial.println(myENS.getTVOC());
+
+	}
+
+	delay(1000);
+}
